5afa569865aa3b94ced5271de47f94f189f7e247
unknown <isa@TO-IP235.v6.vosstaniya6.inner>
2018-11-16 14:49:25 +0300

    summator

--- Tasks/summator.py
+++ Tasks/summator.py
@@ -1,5 +1,48 @@
+# arr = [2,12,1,10,5,7]
+# arr = [2,12,1,10,5,7,'cat']
+class SummatorError(Exception):
+	pass
+
+
+class WrongType(SummatorError):
+	pass
+
+
+class NotList(SummatorError):
+	pass
+
+
+class EmptyInput(SummatorError):
+	pass
+
+
 def sum(input):
-	return
-	
+	s = 0
+
+	if type(input) != list:
+		raise NotList("U give me not list!")
+	for i in input:
+		if type(i) != int:
+			raise WrongType("This is not int!")
+	for i in input:
+		s += i
+
+	return s
+
+
 def avg(input):
-	return
+	s = 0
+	length = len(input)
+
+	if length == 0:
+		raise EmptyInput
+	if type(input) != list:
+		raise NotList("U give me not list!")
+	for i in input:
+		if type(i) != int:
+			raise WrongType("This is not int!")
+
+	for i in input:
+		s += i
+	sr = s / length
+	return sr
