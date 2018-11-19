b8221a513907236a9b2a304cc01f1dd3fa457b6e
Victor Foteev <vfoteev@v6.vosstaniya6.inner>
2018-11-19 13:00:24 +0300

    summator done

--- Tasks/summator.py
+++ Tasks/summator.py
@@ -1,59 +1,27 @@
+class SummatorError(Exception):
+    pass
 
-
-
-
+def is_inp_correct(l):
+    for i in l:
+        if type(i) != int and type(i) != float:
+            return False
+    return True
 
 def sum(input):
+    if not is_inp_correct(input):
+        raise SummatorError("На вход был подан массив с некорректными данными")
     my_sum = 0
     for i in input:
         my_sum += i
-
-        class TypeError(Exception):
-            pass
-
-        class NotTypeError(TypeError):
-            pass
-        if type(input) != list:
-            raise NotTypeError("Input isn`t a list")
     return my_sum
 
 def avg(input):
+    if len(input) == 0:
+        raise SummatorError("На вход был подан пустой массив")
+    if not is_inp_correct(input):
+        raise SummatorError("На вход был подан массив с некорректными данными")
+
     my_avg = sum(input) / len(input)
+
     return my_avg
 
-
-print(sum([1,2,3,4,5]))
-print(avg([1,2,3,4,5]))
-
-# def f_sum(my_list:list)->int:
-#     """
-#     It returns sum of all elements
-#     :return:
-#     """
-#
-#     class TypeError(Exception):
-#         pass
-#     class NotTypeError(TypeError):
-#         pass
-#
-#
-#     if type(my_list) != list:
-#         raise NotTypeError("Input isn`t a list")
-#
-#
-#     my_sum = 0
-#     for i in my_list:
-#         my_sum += i
-#
-#
-#     return my_sum
-#
-# def avg(my_list:list)->int:
-#     """
-#     It returns avg of all elements
-#     :return:
-#     """
-#     my_avg = f_sum(list) / len(my_list)
-#     return my_avg
-#
-# print(f_sum([1,2,3,4,5]))
