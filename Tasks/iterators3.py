404d46816210bce35ec785c61a3a12c364c94d4c
Victor Foteev <vfoteev@v6.vosstaniya6.inner>
2018-11-22 17:04:36 +0300

    iterators3 optimaized

--- Tasks/iterators3.py
+++ Tasks/iterators3.py
@@ -1,6 +1,3 @@
 l = "шел я лесоM вижу мост. Да, на мосту вОрона сохнет. взял еЁ за хвост."
-
-print('. '.join([x.capitalize() for x in [x.lower() for x in (''.join([x.lower() for x in l]).split(". "))]]))
-
-
-
+print(l)
+print('. '.join([x.capitalize() for x in (''.join([x.lower() for x in l]).split(". "))]))
